04c65dd0f3728348df91f7e0b642fa3d182e56d3
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-08-27 21:00:22 +0530

    Add files via upload

---
+++ FIT.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    
+    while (t--) {
+        int x,y,z;
+        scanf("%d", &x);
+       y=(2*x);
+       z=(5*y);
+       printf("%d\n",z);
+    }
+}
