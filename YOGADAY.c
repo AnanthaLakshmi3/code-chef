7f1c22905d7d2a547f2fd25c248ee29ab4ec63b6
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-08-28 19:47:56 +0530

    Add files via upload

---
+++ YOGADAY.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+     int x,y;
+     scanf("%d",&x);
+     if(x>=12){
+      y=x/12;
+     printf("%d",y);
+     }
+     else{
+         printf("%d",0);
+     }
+     return 0;
+}
+
