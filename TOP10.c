7ef92d3d87037ff6b174d1c6ea1df8dc9fcebd38
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-08-28 19:48:56 +0530

    Add files via upload

--- TOP10.c
+++ TOP10.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
 
 int main(void) {
-	// your code goes here
-	int t;
-	scanf("%d",&t);
-	while(t--)
-	{
-	    int x,y;
-	    scanf("%d",&x);
-	    if(x<=10)
-	    {
-	        printf("YES\n");
-	    }
-	    else
-	    {
-	        printf("NO\n");
-	    }
-	}
-
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int x;
+        scanf("%d",&x);
+        if(x<=10)
+        {
+            printf("YES\n");
+        }
+        else{
+            printf("NO\n");
+        }
+    }
 }
 
