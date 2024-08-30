ae4639e27a5d6664ba6f2ff879a236bbad83550f
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-08-30 20:40:48 +0530

    Add files via upload

---
+++ ADVITIYA1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int n;
+	scanf("%d",&n);
+	if(n >= 16 && n <= 18)
+	{
+	    printf("ADVITIYA");
+	}
+	else
+	{
+	    printf("WAITING FOR ADVITIYA");
+	}
+
+}
+
