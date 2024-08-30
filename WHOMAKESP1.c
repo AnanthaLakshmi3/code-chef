fca0ecbfbdc53d70a0382e99a816cabcd42c0195
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-08-30 20:43:29 +0530

    Add files via upload

---
+++ WHOMAKESP1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+    int x,y,z;
+    scanf("%d%d",&x,&y);
+    if(x<=y)
+    {
+        printf("Tyro");
+    }
+    else
+    {
+        printf("Dom");
+    }
+    return 0;
+}
+
