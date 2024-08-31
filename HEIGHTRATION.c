b4b07c78685b34b2e284cf8f6642889d1fb6199c
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-08-31 20:30:32 +0530

    Add files via upload

---
+++ HEIGHTRATION.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+    int x,y;
+    scanf("%d%d",&x,&y);
+    if(x>y){
+       printf("%d",x);
+    }
+    else{
+        printf("%d",y);
+    }
+}
+
