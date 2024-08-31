f42540dc7164a9ad2e353b4448fe6306324a81b4
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-08-31 20:30:51 +0530

    Add files via upload

---
+++ DISCNT.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int x,y;
+        scanf("%d",&x);
+        y=100-x;
+        printf("%d\n",y);
+    }
+}
+
