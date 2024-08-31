6258505c18584ea4b60098c258affeba8b0549f1
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-08-31 20:31:16 +0530

    Add files via upload

---
+++ NEWSPAPER.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+   int x;
+   scanf("%d",&x);
+   if(x==8 || x==9 || x==10)
+   {
+       printf("YES");
+   }
+   else
+   {
+       printf("NO");
+   }
+}
+
